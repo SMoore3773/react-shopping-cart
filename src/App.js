d986a9a80655e85b77baf0b6920107cc27f183c2
SMoore3773 <Smooresculpture@gmail.com>
2020-04-16 23:27:38 -0400

    mvp

--- src/App.js
+++ src/App.js
@@ -14,25 +14,32 @@ function App() {
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
-		console.log(item)
-		setCart([item])
+		setCart([...cart,item]);
 		// add the given item to the cart
 	};
+	const removeItem = item =>{
+		const cartArr = cart;
+		console.log('cartArr in remove item function',cartArr)
+		cartArr.slice(item.id)
+		setCart(cartArr)
+	}
 console.log('cart in app.js',cart);
 	return (
 		
 		<div className="App">
 			<ProductContext.Provider value={{products, addItem}}>
-				<Navigation cart={cart} />
+				<CartContext.Provider value={{cart}}>
+					<Navigation cart={cart} />
 
-				{/* Routes */}
-				<Route exact path="/">
-					<Products  />
-				</Route>
+					{/* Routes */}
+					<Route exact path="/">
+						<Products  />
+					</Route>
 
-				<Route path="/cart">
-					<ShoppingCart cart={cart} />
-				</Route>
+					<Route path="/cart">
+						<ShoppingCart cart={{cart, removeItem}} />
+					</Route>
+				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 		
